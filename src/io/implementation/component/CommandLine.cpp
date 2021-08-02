038f17aa5ab85705c66a8e34cfb623792cc5ddd9
Thorben Louw <thorben.louw@gmail.com>
2021-08-02 12:57:45 +0100

    [TL] Fix CmmandListTests

--- src/io/implementation/component/CommandLine.cpp
+++ src/io/implementation/component/CommandLine.cpp
@@ -10,141 +10,132 @@
  */
 
 #include "CommandLine.h"
-#include <string>
+
 #include <cstring>
 #include <iostream>
-#include "Error.h"
+#include <string>
+
 #include "ArrayDrivers.h"
+#include "Error.h"
 
-namespace cupcfd
-{
-	namespace structure
-	{
-		CommandLine::CommandLine(int argc, char ** argv)
-		{
-			this->parse(argc, argv);
-		}
+namespace cupcfd::structure {
+CommandLine::CommandLine(int argc, char** argv) { this->parse(argc, argv); }
 
-		CommandLine::~CommandLine()
-		{
-			// Nothing to do currently
-		}
+CommandLine::~CommandLine() = default;
 
-		void CommandLine::parse(int argc, char ** argv) {
-			std::string curOpt = "";
+void CommandLine::parse(int argc, char** argv) {
+  std::string curOpt;
 
-			// Skip argv[0]
-			for(int i = 1; i < argc; i++) {
-				// ToDo - Handle case of just a single dash or double dash (i.e. no letters/numbers)
+  // Skip argv[0]
+  for (int i = 1; i < argc; i++) {
+    // ToDo - Handle case of just a single dash or double dash (i.e. no
+    // letters/numbers)
 
-				// Check for a flag option
-				// Accept either a single or double dash
-				bool singleDash = strlen(argv[i]) > 0 && argv[i][0] == '-';
-				bool doubleDash = strlen(argv[i]) > 1 && argv[i][0] == '-' && argv[i][1] == '-';
+    // Check for a flag option
+    // Accept either a single or double dash
+    bool singleDash = strlen(argv[i]) > 0 && argv[i][0] == '-';
+    bool doubleDash =
+        strlen(argv[i]) > 1 && argv[i][0] == '-' && argv[i][1] == '-';
 
-				if(doubleDash) {
-					curOpt = std::string(argv[i]);
-					curOpt.erase(0, 2);
-					this->optionsToArgs[curOpt] = std::vector<std::string>();
-					this->nOptions = this->nOptions + 1;
-				}
-				else if(singleDash) {
-					curOpt = std::string(argv[i]);
-					curOpt.erase(0, 1);
-					this->optionsToArgs[curOpt] = std::vector<std::string>();
-					this->nOptions = this->nOptions + 1;
-				}
-				else {
-					if(curOpt != "") {
-						// Argument to current option
-						this->optionsToArgs[curOpt].push_back(argv[i]);
-					}
-					else {
-						// Error - We have an argument without a option flag
-						// ToDo: Handle appropriately - this is a warning, not a terminal error
-						std::cout << "Warning: Argument " << std::string(argv[i]) << " found without a preceeding flag\n";
-					}
-				}
-			}
-		}
-
-		cupcfd::error::eCodes CommandLine::getBoolArg(std::string flag, int arg, bool * result) {
-			std::string argString;
-			cupcfd::error::eCodes status = this->getStringArg(flag, arg, &argString);
-			CHECK_ECODE(status)
-
-			if(argString == "0") {
-				*result = false;
-			}
-			else if(argString == "1") {
-				*result = true;
-			}
-			else if(argString == "false") {
-				*result = false;
-			}
-			else if(argString == "true") {
-				*result = true;
-			}
-			else {
-				// Error - arg is unsuitable for bool conversion
-				return cupcfd::error::E_CMDLINE_ERR_ARG_INVALID_TYPE;
-			}
-
-			return cupcfd::error::E_SUCCESS;
-		}
-
-		cupcfd::error::eCodes CommandLine::getIntArg(std::string flag, int arg, int * result) {
-			std::string argString;
-			cupcfd::error::eCodes status = this->getStringArg(flag, arg, &argString);
-			CHECK_ECODE(status)
-
-			try {
-				*result = std::stoi(argString, NULL);
-			}
-			catch(std::invalid_argument e) {
-				return cupcfd::error::E_CMDLINE_ERR_ARG_INVALID_TYPE;
-			}
-
-			return cupcfd::error::E_SUCCESS;
-		}
-
-		cupcfd::error::eCodes CommandLine::getDoubleArg(std::string flag, int arg, double * result) {
-			std::string argString;
-			cupcfd::error::eCodes status = this->getStringArg(flag, arg, &argString);
-			CHECK_ECODE(status)
-
-			try {
-				*result = std::stod(argString, NULL);
-			}
-			catch(std::invalid_argument e) {
-				return cupcfd::error::E_CMDLINE_ERR_ARG_INVALID_TYPE;
-			}
-
-			return cupcfd::error::E_SUCCESS;
-		}
-
-		cupcfd::error::eCodes CommandLine::getStringArg(std::string flag, int arg, std::string * result) {
-			// Error Check: Does the flag exist?
-			std::unordered_map<std::string, std::vector<std::string>>::const_iterator search = this->optionsToArgs.find(flag);
-			if(search == this->optionsToArgs.end()) {
-				// Error - Not Found
-				*result = "";
-				return cupcfd::error::E_CMDLINE_ERR_ARG_MISSING_FLAG;
-			}
-
-			// Error Check: Does the flag have sufficient arguments for index arg
-			int size = cupcfd::utility::drivers::safeConvertSizeT<int>(this->optionsToArgs[flag].size());
-			if(arg >= size) {
-				// Error - arg value too high
-				*result = "";
-				return cupcfd::error::E_CMDLINE_ERR_ARG_INVALID_ARG;
-			}
-
-			// Try to convert value
-			std::string argString = this->optionsToArgs[flag][arg];
-			*result = argString;
-
-			return cupcfd::error::E_SUCCESS;
-		}
-	}
+    if (doubleDash) {
+      curOpt = std::string(argv[i]);
+      curOpt.erase(0, 2);
+      this->optionsToArgs[curOpt] = std::vector<std::string>();
+      this->nOptions = this->nOptions + 1;
+    } else if (singleDash) {
+      curOpt = std::string(argv[i]);
+      curOpt.erase(0, 1);
+      this->optionsToArgs[curOpt] = std::vector<std::string>();
+      this->nOptions = this->nOptions + 1;
+    } else {
+      if (!curOpt.empty()) {
+        // Argument to current option
+        this->optionsToArgs[curOpt].push_back(argv[i]);
+      } else {
+        // Error - We have an argument without a option flag
+        // ToDo: Handle appropriately - this is a warning, not a terminal error
+        std::cout << "Warning: Argument " << std::string(argv[i])
+                  << " found without a preceding flag\n";
+      }
+    }
+  }
 }
+
+cupcfd::error::eCodes CommandLine::getBoolArg(std::string flag, int arg,
+                                              bool* result) {
+  std::string argString;
+  cupcfd::error::eCodes status = this->getStringArg(flag, arg, &argString);
+  CHECK_ECODE(status)
+
+  if (argString == "0") {
+    *result = false;
+  } else if (argString == "1") {
+    *result = true;
+  } else if (argString == "false") {
+    *result = false;
+  } else if (argString == "true") {
+    *result = true;
+  } else {
+    // Error - arg is unsuitable for bool conversion
+    return cupcfd::error::E_CMDLINE_ERR_ARG_INVALID_TYPE;
+  }
+
+  return cupcfd::error::E_SUCCESS;
+}
+
+cupcfd::error::eCodes CommandLine::getIntArg(std::string flag, int arg,
+                                             int* result) {
+  std::string argString;
+  cupcfd::error::eCodes status = this->getStringArg(flag, arg, &argString);
+  CHECK_ECODE(status)
+
+  try {
+    *result = std::stoi(argString, NULL);
+  } catch (std::invalid_argument &e) {
+    return cupcfd::error::E_CMDLINE_ERR_ARG_INVALID_TYPE;
+  }
+
+  return cupcfd::error::E_SUCCESS;
+}
+
+cupcfd::error::eCodes CommandLine::getDoubleArg(std::string flag, int arg,
+                                                double* result) {
+  std::string argString;
+  cupcfd::error::eCodes status = this->getStringArg(flag, arg, &argString);
+  CHECK_ECODE(status)
+
+  try {
+    *result = std::stod(argString, NULL);
+  } catch (std::invalid_argument &e) {
+    return cupcfd::error::E_CMDLINE_ERR_ARG_INVALID_TYPE;
+  }
+
+  return cupcfd::error::E_SUCCESS;
+}
+
+cupcfd::error::eCodes CommandLine::getStringArg(std::string flag, int arg,
+                                                std::string* result) {
+  // Error Check: Does the flag exist?
+  auto search = this->optionsToArgs.find(flag);
+  if (search == this->optionsToArgs.end()) {
+    // Error - Not Found
+    *result = "";
+    return cupcfd::error::E_CMDLINE_ERR_ARG_MISSING_FLAG;
+  }
+
+  // Error Check: Does the flag have sufficient arguments for index arg
+  int size = cupcfd::utility::drivers::safeConvertSizeT<int>(
+      this->optionsToArgs[flag].size());
+  if (arg >= size) {
+    // Error - arg value too high
+    *result = "";
+    return cupcfd::error::E_CMDLINE_ERR_ARG_INVALID_ARG;
+  }
+
+  // Try to convert value
+  std::string argString = this->optionsToArgs[flag][arg];
+  *result = argString;
+
+  return cupcfd::error::E_SUCCESS;
+}
+}  // namespace cupcfd::structure
--- tests/io/implementation/component/CommandLineTests.cpp
+++ tests/io/implementation/component/CommandLineTests.cpp
@@ -13,266 +13,243 @@
  */
 
 #define BOOST_TEST_MODULE CommandLine
-#include <boost/test/unit_test.hpp>
 #include <boost/test/tools/output_test_stream.hpp>
-
+#include <boost/test/unit_test.hpp>
 #include <stdexcept>
-#include "CommandLine.h"
 #include <string>
+
+#include "CommandLine.h"
 
 using namespace cupcfd::structure;
 
 // === Constructor/Deconstructor ===
 // Test 1: Check the constructor runs without crashing
-BOOST_AUTO_TEST_CASE(constructor_test1)
-{
-    // Rather than providing command line args to the test binary,
-    // we will hardcode argc and argv inside the tests
+BOOST_AUTO_TEST_CASE(constructor_test1) {
+  // Rather than providing command line args to the test binary,
+  // we will hardcode argc and argv inside the tests
 
-    int argc = 1;
-    char argv[1][10] = {{"binName"}};
-    CommandLine opts(argc, (char**) argv);
+  int argc = 1;
+  char argv[1][10] = {{"binName"}};
+  CommandLine opts(argc, (char **)argv);
 }
 
 // === parse ===
 // Test 1: Parse is triggered by the constructor:
 // Check the internal contents
 // Test 1:
-BOOST_AUTO_TEST_CASE(parse_test1)
-{
-    int argc = 3;
+BOOST_AUTO_TEST_CASE(parse_test1) {
+  int argc = 3;
 
-    char ** argv = (char **) malloc(sizeof(char *) * 3);
-    for(int i = 0; i < 3; i++)
-    {
-    	argv[i] = (char *) malloc(sizeof(char) * 10);
-    }
-    strcpy(argv[0], "binName");
-    strcpy(argv[1], "--test");
-    strcpy(argv[2], "4");
+  char **argv = (char **)malloc(sizeof(char *) * 3);
+  for (int i = 0; i < 3; i++) {
+    argv[i] = (char *)malloc(sizeof(char) * 10);
+  }
+  strcpy(argv[0], "binName");
+  strcpy(argv[1], "--test");
+  strcpy(argv[2], "4");
 
-    CommandLine opts(argc, argv);
-    BOOST_CHECK_EQUAL(opts.nOptions, 1);
-    BOOST_CHECK_EQUAL(opts.optionsToArgs.size(), 1);
+  CommandLine opts(argc, argv);
+  BOOST_CHECK_EQUAL(opts.nOptions, 1);
+  BOOST_CHECK_EQUAL(opts.optionsToArgs.size(), 1);
 }
 
 // === getStringArg ===
-BOOST_AUTO_TEST_CASE(getStringArg_test1)
-{
-	cupcfd::error::eCodes status;
+BOOST_AUTO_TEST_CASE(getStringArg_test1) {
+  cupcfd::error::eCodes status;
 
-    int argc = 3;
+  int argc = 3;
 
-    char ** argv = (char **) malloc(sizeof(char *) * 3);
-    for(int i = 0; i < 3; i++)
-    {
-    	argv[i] = (char *) malloc(sizeof(char) * 10);
-    }
-    strcpy(argv[0], "binName");
-    strcpy(argv[1], "--test");
-    strcpy(argv[2], "abcde");
+  char **argv = (char **)malloc(sizeof(char *) * 3);
+  for (int i = 0; i < 3; i++) {
+    argv[i] = (char *)malloc(sizeof(char) * 10);
+  }
+  strcpy(argv[0], "binName");
+  strcpy(argv[1], "--test");
+  strcpy(argv[2], "abcde");
 
-    CommandLine opts(argc, argv);
+  CommandLine opts(argc, argv);
 
-    std::string result;
-    status = opts.getStringArg("test", 0, &result);
-    BOOST_CHECK_EQUAL(status, cupcfd::error::E_SUCCESS);
-    BOOST_CHECK_EQUAL(result, "abcde");
+  std::string result;
+  status = opts.getStringArg("test", 0, &result);
+  BOOST_CHECK_EQUAL(status, cupcfd::error::E_SUCCESS);
+  BOOST_CHECK_EQUAL(result, "abcde");
 }
 
-BOOST_AUTO_TEST_CASE(getStringArg_test2)
-{
-	cupcfd::error::eCodes status;
+BOOST_AUTO_TEST_CASE(getStringArg_test2) {
+  cupcfd::error::eCodes status;
 
-    int argc = 4;
+  int argc = 4;
 
-    char ** argv = (char **) malloc(sizeof(char *) * 4);
-    for(int i = 0; i < 4; i++)
-    {
-    	argv[i] = (char *) malloc(sizeof(char) * 10);
-    }
-    strcpy(argv[0], "binName");
-    strcpy(argv[1], "--test");
-    strcpy(argv[2], "abcde");
-    strcpy(argv[2], "fghijk");
+  char **argv = (char **)malloc(sizeof(char *) * 4);
+  for (int i = 0; i < 4; i++) {
+    argv[i] = (char *)malloc(sizeof(char) * 10);
+  }
+  strcpy(argv[0], "binName");
+  strcpy(argv[1], "--test");
+  strcpy(argv[2], "abcde");
+  strcpy(argv[3], "fghijk");
 
-    CommandLine opts(argc, argv);
+  CommandLine opts(argc, argv);
 
-    std::string result;
-    status = opts.getStringArg("test", 1, &result);
-    BOOST_CHECK_EQUAL(status, cupcfd::error::E_SUCCESS);
-    BOOST_CHECK_EQUAL(result, "fghijk");
+  std::string result;
+  status = opts.getStringArg("test", 1, &result);
+  BOOST_CHECK_EQUAL(status, cupcfd::error::E_SUCCESS);
+  BOOST_CHECK_EQUAL(result, std::string("fghijk"));
 }
 
-BOOST_AUTO_TEST_CASE(getStringArg_test3)
-{
-	cupcfd::error::eCodes status;
+BOOST_AUTO_TEST_CASE(getStringArg_test3) {
+  cupcfd::error::eCodes status;
 
-    int argc = 4;
+  int argc = 4;
 
-    char ** argv = (char **) malloc(sizeof(char *) * 4);
-    for(int i = 0; i < 4; i++)
-    {
-    	argv[i] = (char *) malloc(sizeof(char) * 10);
-    }
-    strcpy(argv[0], "binName");
-    strcpy(argv[1], "--test");
-    strcpy(argv[2], "abcde");
-    strcpy(argv[2], "fghijk");
+  char **argv = (char **)malloc(sizeof(char *) * 4);
+  for (int i = 0; i < 4; i++) {
+    argv[i] = (char *)malloc(sizeof(char) * 10);
+  }
+  strcpy(argv[0], "binName");
+  strcpy(argv[1], "--test");
+  strcpy(argv[2], "abcde");
+  strcpy(argv[2], "fghijk");
 
-    CommandLine opts(argc, argv);
+  CommandLine opts(argc, argv);
 
-    std::string result;
-    status = opts.getStringArg("missing", 0, &result);
-    BOOST_CHECK_EQUAL(status, cupcfd::error::E_CMDLINE_ERR_ARG_MISSING_FLAG);
+  std::string result;
+  status = opts.getStringArg("missing", 0, &result);
+  BOOST_CHECK_EQUAL(status, cupcfd::error::E_CMDLINE_ERR_ARG_MISSING_FLAG);
 }
 
-BOOST_AUTO_TEST_CASE(getStringArg_test4)
-{
-	cupcfd::error::eCodes status;
+BOOST_AUTO_TEST_CASE(getStringArg_test4) {
+  cupcfd::error::eCodes status;
 
-    int argc = 4;
+  int argc = 4;
 
-    char ** argv = (char **) malloc(sizeof(char *) * 4);
-    for(int i = 0; i < 4; i++)
-    {
-    	argv[i] = (char *) malloc(sizeof(char) * 10);
-    }
-    strcpy(argv[0], "binName");
-    strcpy(argv[1], "--test");
-    strcpy(argv[2], "abcde");
-    strcpy(argv[2], "fghijk");
+  char **argv = (char **)malloc(sizeof(char *) * 4);
+  for (int i = 0; i < 4; i++) {
+    argv[i] = (char *)malloc(sizeof(char) * 10);
+  }
+  strcpy(argv[0], "binName");
+  strcpy(argv[1], "--test");
+  strcpy(argv[2], "abcde");
+  strcpy(argv[2], "fghijk");
 
-    CommandLine opts(argc, argv);
+  CommandLine opts(argc, argv);
 
-    std::string result;
-    status = opts.getStringArg("test", 3, &result);
-    BOOST_CHECK_EQUAL(status, cupcfd::error::E_CMDLINE_ERR_ARG_INVALID_ARG);
+  std::string result;
+  status = opts.getStringArg("test", 3, &result);
+  BOOST_CHECK_EQUAL(status, cupcfd::error::E_CMDLINE_ERR_ARG_INVALID_ARG);
 }
 
 // === getBoolArg ===
 // Test 1: Retrieve a boolean argument from a flag with only one argument, true
-BOOST_AUTO_TEST_CASE(getBoolArg_test1)
-{
-	cupcfd::error::eCodes status;
+BOOST_AUTO_TEST_CASE(getBoolArg_test1) {
+  cupcfd::error::eCodes status;
 
-    int argc = 3;
+  int argc = 3;
 
-    char ** argv = (char **) malloc(sizeof(char *) * 3);
-    for(int i = 0; i < 3; i++)
-    {
-    	argv[i] = (char *) malloc(sizeof(char) * 10);
-    }
-    strcpy(argv[0], "binName");
-    strcpy(argv[1], "--test");
-    strcpy(argv[2], "true");
+  char **argv = (char **)malloc(sizeof(char *) * 3);
+  for (int i = 0; i < 3; i++) {
+    argv[i] = (char *)malloc(sizeof(char) * 10);
+  }
+  strcpy(argv[0], "binName");
+  strcpy(argv[1], "--test");
+  strcpy(argv[2], "true");
 
-    CommandLine opts(argc, argv);
+  CommandLine opts(argc, argv);
 
-    bool result;
-    status = opts.getBoolArg("test", 0, &result);
-    BOOST_CHECK_EQUAL(status, cupcfd::error::E_SUCCESS);
-    BOOST_CHECK_EQUAL(result, true);
+  bool result;
+  status = opts.getBoolArg("test", 0, &result);
+  BOOST_CHECK_EQUAL(status, cupcfd::error::E_SUCCESS);
+  BOOST_CHECK_EQUAL(result, true);
 }
 
 // Test 2: Retrieve a boolean argument from a flag with only one argument, false
-BOOST_AUTO_TEST_CASE(getBoolArg_test2)
-{
-	cupcfd::error::eCodes status;
+BOOST_AUTO_TEST_CASE(getBoolArg_test2) {
+  cupcfd::error::eCodes status;
 
-    int argc = 3;
+  int argc = 3;
 
-    char ** argv = (char **) malloc(sizeof(char *) * 3);
-    for(int i = 0; i < 3; i++)
-    {
-    	argv[i] = (char *) malloc(sizeof(char) * 10);
-    }
-    strcpy(argv[0], "binName");
-    strcpy(argv[1], "--test");
-    strcpy(argv[2], "false");
+  char **argv = (char **)malloc(sizeof(char *) * 3);
+  for (int i = 0; i < 3; i++) {
+    argv[i] = (char *)malloc(sizeof(char) * 10);
+  }
+  strcpy(argv[0], "binName");
+  strcpy(argv[1], "--test");
+  strcpy(argv[2], "false");
 
-    CommandLine opts(argc, argv);
+  CommandLine opts(argc, argv);
 
-    bool result;
-    status = opts.getBoolArg("test", 0, &result);
-    BOOST_CHECK_EQUAL(status, cupcfd::error::E_SUCCESS);
-    BOOST_CHECK_EQUAL(result, false);
+  bool result;
+  status = opts.getBoolArg("test", 0, &result);
+  BOOST_CHECK_EQUAL(status, cupcfd::error::E_SUCCESS);
+  BOOST_CHECK_EQUAL(result, false);
 }
 
 // Test 2: Retrieve a boolean argument from a flag with only one argument, 1
-BOOST_AUTO_TEST_CASE(getBoolArg_test3)
-{
-	cupcfd::error::eCodes status;
+BOOST_AUTO_TEST_CASE(getBoolArg_test3) {
+  cupcfd::error::eCodes status;
 
-    int argc = 3;
+  int argc = 3;
 
-    char ** argv = (char **) malloc(sizeof(char *) * 3);
-    for(int i = 0; i < 3; i++)
-    {
-    	argv[i] = (char *) malloc(sizeof(char) * 10);
-    }
-    strcpy(argv[0], "binName");
-    strcpy(argv[1], "--test");
-    strcpy(argv[2], "1");
+  char **argv = (char **)malloc(sizeof(char *) * 3);
+  for (int i = 0; i < 3; i++) {
+    argv[i] = (char *)malloc(sizeof(char) * 10);
+  }
+  strcpy(argv[0], "binName");
+  strcpy(argv[1], "--test");
+  strcpy(argv[2], "1");
 
-    CommandLine opts(argc, argv);
+  CommandLine opts(argc, argv);
 
-    bool result;
-    status = opts.getBoolArg("test", 0, &result);
-    BOOST_CHECK_EQUAL(status, cupcfd::error::E_SUCCESS);
-    BOOST_CHECK_EQUAL(result, true);
+  bool result;
+  status = opts.getBoolArg("test", 0, &result);
+  BOOST_CHECK_EQUAL(status, cupcfd::error::E_SUCCESS);
+  BOOST_CHECK_EQUAL(result, true);
 }
 
 // Test 2: Retrieve a boolean argument from a flag with only one argument, 0
-BOOST_AUTO_TEST_CASE(getBoolArg_test4)
-{
-	cupcfd::error::eCodes status;
+BOOST_AUTO_TEST_CASE(getBoolArg_test4) {
+  cupcfd::error::eCodes status;
 
-    int argc = 3;
+  int argc = 3;
 
-    char ** argv = (char **) malloc(sizeof(char *) * 3);
-    for(int i = 0; i < 3; i++)
-    {
-    	argv[i] = (char *) malloc(sizeof(char) * 10);
-    }
-    strcpy(argv[0], "binName");
-    strcpy(argv[1], "--test");
-    strcpy(argv[2], "0");
+  char **argv = (char **)malloc(sizeof(char *) * 3);
+  for (int i = 0; i < 3; i++) {
+    argv[i] = (char *)malloc(sizeof(char) * 10);
+  }
+  strcpy(argv[0], "binName");
+  strcpy(argv[1], "--test");
+  strcpy(argv[2], "0");
 
-    CommandLine opts(argc, argv);
+  CommandLine opts(argc, argv);
 
-    bool result;
-    status = opts.getBoolArg("test", 0, &result);
-    BOOST_CHECK_EQUAL(status, cupcfd::error::E_SUCCESS);
-    BOOST_CHECK_EQUAL(result, false);
+  bool result;
+  status = opts.getBoolArg("test", 0, &result);
+  BOOST_CHECK_EQUAL(status, cupcfd::error::E_SUCCESS);
+  BOOST_CHECK_EQUAL(result, false);
 }
 
-// Test 2: Retrieve a boolean argument from a flag with only one argument, non-boolean
-BOOST_AUTO_TEST_CASE(getBoolArg_test5)
-{
-	cupcfd::error::eCodes status;
+// Test 2: Retrieve a boolean argument from a flag with only one argument,
+// non-boolean
+BOOST_AUTO_TEST_CASE(getBoolArg_test5) {
+  cupcfd::error::eCodes status;
 
-    int argc = 3;
+  int argc = 3;
 
-    char ** argv = (char **) malloc(sizeof(char *) * 3);
-    for(int i = 0; i < 3; i++)
-    {
-    	argv[i] = (char *) malloc(sizeof(char) * 10);
-    }
-    strcpy(argv[0], "binName");
-    strcpy(argv[1], "--test");
-    strcpy(argv[2], "abc");
+  char **argv = (char **)malloc(sizeof(char *) * 3);
+  for (int i = 0; i < 3; i++) {
+    argv[i] = (char *)malloc(sizeof(char) * 10);
+  }
+  strcpy(argv[0], "binName");
+  strcpy(argv[1], "--test");
+  strcpy(argv[2], "abc");
 
-    CommandLine opts(argc, argv);
+  CommandLine opts(argc, argv);
 
-    bool result;
-    status = opts.getBoolArg("test", 0, &result);
-    BOOST_CHECK_EQUAL(status, cupcfd::error::E_CMDLINE_ERR_ARG_INVALID_TYPE);
+  bool result;
+  status = opts.getBoolArg("test", 0, &result);
+  BOOST_CHECK_EQUAL(status, cupcfd::error::E_CMDLINE_ERR_ARG_INVALID_TYPE);
 }
-
 
 // === getIntArg ===
 
-
 // === getDoubleArg ===
-
